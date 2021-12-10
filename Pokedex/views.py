78e697241cd10a1eb68348d5cd18fe2681f2e82b
Quentin Andreani <quent@mbp-de-quentin.home>
2021-12-10 23:36:34 +0100

    Amélioration du templates et finalisation

--- Pokedex/views.py
+++ Pokedex/views.py
@@ -3,9 +3,13 @@ import requests
 
 
 def index(request, id):
+    # On initialise le context
+    context = {}
+    # On initialise le retour des erreurs
+    context["error"] = ""
+
+    # Système de vérification d'url pour la fonction de recherche
     check = str(request).find("=")
-
-
     if check != -1:
         test = str(request).split("=")
         numPoke = test[1]
@@ -15,28 +19,31 @@ def index(request, id):
                 num = num + c
         id = num
 
-    # On initialise le context
-    context = {}
-    # On initialise le retour des erreurs
-    context["error"] = ""
-    # Vérification de l'id afin de n'utiliser que les 151 premier Pokémon
+    # Récupération de l'ID du Pokémon
     id = int(id)
+    # On le stock à + 1 ou - 1 pour le système de navigation
     context["next"] = str(id + 1)
     context["previous"] = str(id - 1)
+    # L'API PokeAPI envoie les pokémon à partir de 0 mais nous voulons à partir de 1
     id = id - 1
 
+    # Petite vérification de l'id pour le système de navigation si on arrive au 151ème Pokémon
     if id > 149:
         context["last"] = 1
-
+    # Petite vérification de l'id pour le système de navigation si on est au premier Pokémon
     if id < 1:
         context["first"] = 1
-
+    # On utilise la vue uniquement si on est dans les 151 premiers Pokémon
     if (id < 151 and id > -1):
+        # On transforme l'ID en String pour l'url
         id = str(id)
-
+        # On récupère le pokémon via l'API
         r = requests.get("https://pokeapi.co/api/v2/pokemon?offset=" + id + "&limit=1")
+        # On récupère le résultat
         results = r.json()
 
+        # À partir d'ici tout une phase de récupération d'information
+        # On récupère le nom et l'url du Pokémon pour ensuite récupérer ses informations
         for i in results['results']:
             context['name'] = i['name'].capitalize()
             context['url'] = i['url']
@@ -44,8 +51,12 @@ def index(request, id):
         p = requests.get(context['url'])
         pokeContent = p.json()
 
+        types = {}
         for t in pokeContent['types']:
-            context['poketypes'] = t['type']['name'].capitalize()
+            name = t['type']['name'].capitalize()
+            types[name] = name.capitalize()
+
+        context["poketypes"] = types
 
         stats = pokeContent['stats']
 
@@ -58,10 +69,9 @@ def index(request, id):
         context['specialAttack'] = stats[3]["base_stat"]
         context['specialDefense'] = stats[4]["base_stat"]
 
+        # On récupère l'image
         images = pokeContent['sprites']
         context['sprite'] = images["front_default"]
-
-        # print(context)
     else:
         context["error"] = "Erreur d'id de Pokemon veuillez choisir un pokémon entre 1 et 151"
 
